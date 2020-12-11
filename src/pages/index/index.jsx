2a95d76fba19dc08eccef77a0500b7fd6e2c1844
lupei <lupei@linghit.com>
2020-12-11 19:20:24 +0800

    ...

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,4 +1,4 @@
-import React, {useState,useRef } from 'react';
+import React, {useState,useRef,useReducer  } from 'react';
 import Hello from './components/hello';
 import Word from './components/word';
 import ThemeContext from './components/context';
@@ -16,6 +16,16 @@ const Index = () => {
             console.log(h5Ref,'ddd')
             console.log(myRef.current.innerHTML); // => hello word
         };
+        const [sum, dispatch] = useReducer((state, action) => {
+            switch (action.type) {
+                case 'add':
+                    return state + action.number;
+                case 'reduce':
+                return state - action.number;
+                default:
+                return state;
+            }
+          }, []);
     return (
         <ThemeContext.Provider
         value={{
@@ -24,12 +34,19 @@ const Index = () => {
         >
         <div className='index-page'>
             父组件index
-                <Hello  handleClick={handleClick} count={count}></Hello>
+                <Hello  handlesumClick={handleClick} count={count}></Hello>
                 <Word ref={h5Ref}></Word>
             <div ref={myRef} onClick={clicktoTest}>
                 hello word
             </div>
         </div>
+
+        <button onClick={() => dispatch({type:'add',number:1})}>
+        Add {sum}
+      </button>
+      <button onClick={() => dispatch({type:'reduce',number:1})}>
+      reduce {sum}
+      </button>
         </ThemeContext.Provider>
     );
 };
