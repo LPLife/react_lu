54e40b065498bdc70d738af315eb7a6964b30e7f
lupei <lupei@linghit.com>
2020-12-25 19:26:24 +0800

    ...

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,4 +1,4 @@
-import React, {useState,useRef,useReducer  } from 'react';
+import React, {useState, useRef, useReducer} from 'react';
 import Hello from './components/hello';
 import Word from './components/word';
 import ThemeContext from './components/context';
@@ -13,40 +13,42 @@ const Index = () => {
         myRef = useRef(),
         h5Ref = useRef(),
         clicktoTest = () => {
-            console.log(h5Ref,'ddd')
+            console.log(h5Ref, 'ddd');
             console.log(myRef.current.innerHTML); // => hello word
         };
-        const [sum, dispatch] = useReducer((state, action) => {
-            switch (action.type) {
-                case 'add':
-                    return state + action.number;
-                case 'reduce':
-                return state - action.number;
-                default:
+    /**
+     * useReducer接受一个reducer函数作为参数，以及一个初始值。reducer接收​两个参数
+     *​一个是当前的state，一个action（动作），返回最新的state
+     */
+    const [sum, dispatch] = useReducer((state, action) => {
+        switch (action.type) {
+            case 'add':
+                return state + 1;
+            case 'reduce':
+                return state - 1;
+            default:
                 return state;
-            }
-          }, []);
+        }
+    }, 0);
     return (
         <ThemeContext.Provider
-        value={{
-            username: 'superawesome',
-        }}
+            value={{
+                username: 'superawesome',
+                sumState: sum,
+                sumDispatch: dispatch
+            }}
         >
-        <div className='index-page'>
-            父组件index
-                <Hello  handlesumClick={handleClick} count={count}></Hello>
+            <div className='index-page'>
+                父组件index
+                <Hello handlesumClick={handleClick} count={count}></Hello>
                 <Word ref={h5Ref}></Word>
-            <div ref={myRef} onClick={clicktoTest}>
-                hello word
+                <div ref={myRef} onClick={clicktoTest}>
+                    hello word
+                </div>
             </div>
-        </div>
 
-        <button onClick={() => dispatch({type:'add',number:1})}>
-        Add {sum}
-      </button>
-      <button onClick={() => dispatch({type:'reduce',number:1})}>
-      reduce {sum}
-      </button>
+            <button onClick={() => dispatch({type: 'add'})}>Add {sum}</button>
+            <button onClick={() => dispatch({type: 'reduce'})}>reduce {sum}</button>
         </ThemeContext.Provider>
     );
 };
